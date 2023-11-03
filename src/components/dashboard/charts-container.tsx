abd30140baeaf39b70f09c2ce83d8cfa4bc2a9e9
Dhaval Rupapara <rupaparadhaval1234@gmail.com>
2023-11-03 15:03:34 +0530

    integrating pie chart

--- src/components/dashboard/charts-container.tsx
+++ src/components/dashboard/charts-container.tsx
@@ -10,7 +10,13 @@ export default function ChartsContainer() {
     <div className="space-y-3 md:space-y-6">
       <h3 className="text-center">Monthly Applications</h3>
       <Tabs defaultValue="bar-chart" className="flex flex-col">
-        <TabsList className="mx-auto grid h-fit w-[min(100%,_300px)] grid-cols-3 p-3">
+        <TabsList className="mx-auto grid h-fit w-[min(100%,_400px)] grid-cols-3 p-3">
+        <TabsTrigger value="pie-chart">
+            <div className="flex items-center gap-1">
+              <PieChartIcon aria-hidden="true" />
+              <span>Pie Chart</span>
+            </div>
+          </TabsTrigger>
           <TabsTrigger value="bar-chart">
             <div className="flex items-center gap-1">
               <BarChart2Icon aria-hidden="true" />
@@ -23,18 +29,15 @@ export default function ChartsContainer() {
               <span>Line Chart</span>
             </div>
           </TabsTrigger>
-          <TabsTrigger value="line-chart">
-            <div className="flex items-center gap-1">
-              <PieChartIcon aria-hidden="true" />
-              <span>Pie Chart</span>
-            </div>
-          </TabsTrigger>
         </TabsList>
+        <TabsContent value="pie-chart">
+          <StatsPieChart />
+        </TabsContent>
         <TabsContent value="bar-chart">
           <StatsBarChart />
         </TabsContent>
         <TabsContent value="line-chart">
-          <StatsPieChart />
+          <StatsLineChart />
         </TabsContent>
       </Tabs>
     </div>
